701aaaeabc56001fdd870bca825f0c7f1c9cccab
fred231084g <83791194+fred231084g@users.noreply.github.com>
2025-11-26 01:56:58 +0100

    Delete flexvolume/windows directory

--- flexvolume/windows/vs_build/buildscript.cmd
+++
@@ -1,2 +0,0 @@
-CALL C:\\BuildTools\\Common7\\Tools\\VsDevCmd.bat
-CALL MSBuild c:\code\utils\iscsiHelper\iscsiHelper.sln /p:OutDir=c:\code\plugins\microsoft.com~iscsi.cmd\;Configuration=Release;Platform=x64
